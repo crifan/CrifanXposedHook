50655de96e6ab4bb95c118fec49e5c1c2e11c6aa
crifanli <admin@crifan.com>
2023-08-11 21:44:31 +0800

    added code and readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# CrifanXposedHook
+
+## 代码仓库
+
+https://github.com/crifan/CrifanXposedHook
+
+https://github.com/crifan/CrifanXposedHook.git
+
+## 项目功能
+
+Xposed插件demo演示项目，Android Studio项目代码
+
+## 效果
+
+* EdXposed中能查看到Xposed模块
+  * ![edxposed_xposed_module_enabled](doc/assets/img/edxposed_xposed_module_enabled.png)
+* 从EdXposed的日志中能查看到Hook代码的log日志
+  * ![edxposed_module_log_hooked_2](doc/assets/img/edxposed_module_log_hooked_2.png)
---
+++ app/src/main/java/com/example/crifanxposedhook/XposedHook.java
@@ -0,0 +1,10 @@
+package com.example.crifanxposedhook;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class XposedHook implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("XposedHook Loaded app: " + lpparam.packageName);
+    }
+}
