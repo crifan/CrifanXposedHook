dd53168c407778e4ab07a47ebad974487e32f963
crifan <admin@crifan.com>
2024-12-17 11:27:19 +0800

    1. update to makesure xposed module work

--- app/src/main/java/com/example/crifanxposedhook/XposedHook.java
+++ app/src/main/java/com/example/crifanxposedhook/XposedHook.java
@@ -1,10 +1,11 @@
 package com.example.crifanxposedhook;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedHook implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("XposedHook Loaded app: " + lpparam.packageName);
+        XposedBridge.log("XposedHook Loaded app: packageName=" + lpparam.packageName + ", processName=" + lpparam.processName);
     }
 }
